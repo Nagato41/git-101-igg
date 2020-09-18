48981464a17a3c575878950839ff9ed7fafb72aa
AnuKPto <37680296+Nagato41@users.noreply.github.com>
2020-09-18 11:08:07 +0700

    add

--- my-resume/App.js
+++ my-resume/App.js
@@ -2,14 +2,8 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import ResumeFormScreen from './screens/ResumeForm';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
@@ -17,7 +11,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ResumeForm" options={{ title:'Resume Form'}} component={ResumeFormScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
